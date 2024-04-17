0d11f22d8bca44d899fef5c35e2f00f04014ae7c
“sobhan”912 <sobhan912@gmail.com>
2024-04-17 23:06:13 +0200

    first changes

--- lib/views/utils/AppColor.dart
+++ lib/views/utils/AppColor.dart
@@ -8,6 +8,7 @@ class AppColor {
   static Color secondary = Color(0xFFEDE5CC);
   static Color whiteSoft = Color(0xFFF8F8F8);
   static Color BlackSoft = Color(0xFF0C0C0C);
+  static Color pink = Color(0xFF0C0C0C);
   // static LinearGradient bottomShadow = LinearGradient(colors: [Color(0xFF107873).withOpacity(0.2), Color(0xFF107873).withOpacity(0)], begin: Alignment.bottomCenter, end: Alignment.topCenter);
   // static LinearGradient linearBlackBottom = LinearGradient(colors: [Colors.black.withOpacity(0.45), Colors.black.withOpacity(0)], begin: Alignment.bottomCenter, end: Alignment.topCenter);
   // static LinearGradient linearBlackTop = LinearGradient(colors: [Colors.black.withOpacity(0.5), Colors.transparent], begin: Alignment.topCenter, end: Alignment.bottomCenter);
