eca5d8990ebac370d5cc9e3097bb3837c57c9303
“sobhan”912 <sobhan912@gmail.com>
2024-04-17 23:06:34 +0200

    removed comments

--- lib/views/utils/AppColor.dart
+++ lib/views/utils/AppColor.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static Color primary = Color(0xFF212121);
+
   // static Color primary = Color(0xFF094542);
   static Color primarySoft = Color(0xFF212121);
   static Color primaryExtraSoft = Color(0xFFEEF4F4);
   static Color secondary = Color(0xFFEDE5CC);
   static Color whiteSoft = Color(0xFFF8F8F8);
   static Color BlackSoft = Color(0xFF0C0C0C);
-  static Color pink = Color(0xFF0C0C0C);
-  // static LinearGradient bottomShadow = LinearGradient(colors: [Color(0xFF107873).withOpacity(0.2), Color(0xFF107873).withOpacity(0)], begin: Alignment.bottomCenter, end: Alignment.topCenter);
-  // static LinearGradient linearBlackBottom = LinearGradient(colors: [Colors.black.withOpacity(0.45), Colors.black.withOpacity(0)], begin: Alignment.bottomCenter, end: Alignment.topCenter);
-  // static LinearGradient linearBlackTop = LinearGradient(colors: [Colors.black.withOpacity(0.5), Colors.transparent], begin: Alignment.topCenter, end: Alignment.bottomCenter);
 }
